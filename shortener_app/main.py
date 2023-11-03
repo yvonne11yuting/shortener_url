6a5754f2dc4ed176f78da6c5faea6e6447111156
yvonne11yuting <yvonne11yuting@gmail.com>
2023-11-03 11:52:00 +0800

    update count and delete

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -45,6 +45,7 @@ def forward_to_target_url(
     url_key: str, request: Request, db: Session = Depends(get_db)
 ):
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        crud.update_db_click(db=db, db_url=db_url)
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
@@ -64,5 +65,14 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
 def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
     if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
         return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
+
+
+@app.delete("/admin/{secret_key}")
+def delete_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.deactivate_db_url_by_secret_key(db, secret_key=secret_key):
+        message = f"Successfully deleted shortened URL for '{db_url.target_url}'"
+        return {"detail": message}
     else:
         raise_not_found(request)
